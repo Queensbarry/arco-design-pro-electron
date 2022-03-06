a45482ee9dbe1c2194023fe9862b6b9474c8f4b2
Queensbarry <queensbarry@foxmail.com>
2022-03-06 19:12:22 +0800

    feat: update for production mode

--- public/preload.js
+++ public/preload.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
