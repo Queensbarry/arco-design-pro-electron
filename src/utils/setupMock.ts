f0672fb92d61211f8be11e016ad14d3840d8954f
Queensbarry <queensbarry@foxmail.com>
2022-03-06 22:08:15 +0800

    fixed: mock always setup

--- src/utils/setupMock.ts
+++ src/utils/setupMock.ts
@@ -1,5 +1,6 @@
 export default (config: { mock?: boolean; setup: () => void }) => {
-  const { mock = process.env.NODE_ENV === 'development', setup } = config;
+  // IMPORTANT: MUST CHANGE `mock` TO `false` IF USED FOR PRODUCTION
+  const { mock = true, setup } = config;
   if (mock === false) return;
   setup();
 };
